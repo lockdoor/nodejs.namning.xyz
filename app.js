a4a8e882712939b438715fbd037ecb5ce1af06ac
lockdoor <lockdoor@gmail.com>
2021-06-27 11:53:39 +0700

    main

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-var port = 3000;
+var port = 80;
 app.listen(port,()=>console.log('node start on port', port));
 
 module.exports = app;
